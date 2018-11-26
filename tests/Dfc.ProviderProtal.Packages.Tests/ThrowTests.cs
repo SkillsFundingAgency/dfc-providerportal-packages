9f8b80994a5455855c92d0c1739807ac506efca4
Mahmood Akhtar <mahmood233@hotmail.com>
2018-11-26 15:51:33 +0000

    Adding more unit tests to ThrowTests.cs

--- tests/Dfc.ProviderProtal.Packages.Tests/ThrowTests.cs
+++ tests/Dfc.ProviderProtal.Packages.Tests/ThrowTests.cs
@@ -71,5 +71,82 @@ namespace Dfc.ProviderProtal.Packages.Tests
             // assert
             Assert.Null(expected);
         }
+
+        [Fact]
+        public void Throw_IfNullOrEmpty_WithArgumentAsNullAndNameAsNull_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            string name = null;
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrEmpty(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrEmpty_WithArgumentAsNullAndNameAsEmpty_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            string name = string.Empty;
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrEmpty(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrEmpty_WithArgumentAsNullAndNameAsWhitespace_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            string name = " ";
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrEmpty(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrEmpty_OfT_WithArgumentAsNull_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            var name = nameof(argument);
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrEmpty(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrEmpty_OfT_WithArgumentAsEmpty_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = string.Empty;
+            var name = nameof(argument);
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrEmpty(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrEmpty_OfT_WithArgumentAsNotEmpty_DoesNotThrowArgumentException()
+        {
+            // arrange
+            string argument = "some value";
+            var name = nameof(argument);
+            ArgumentException expected = null;
+
+            //act
+            try
+            {
+                Throw.IfNull(argument, name);
+            }
+            catch (ArgumentException e)
+            {
+                expected = e;
+            }
+
+            // assert
+            Assert.Null(expected);
+        }
     }
 }
