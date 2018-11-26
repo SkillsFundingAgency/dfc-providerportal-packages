e9d94a9144a8ab0469baf7e31350493af97085c4
Mahmood Akhtar <mahmood233@hotmail.com>
2018-11-26 16:02:04 +0000

    Ading more unit tests and fixing the naming of some others.

--- tests/Dfc.ProviderProtal.Packages.Tests/ThrowTests.cs
+++ tests/Dfc.ProviderProtal.Packages.Tests/ThrowTests.cs
@@ -106,7 +106,7 @@ namespace Dfc.ProviderProtal.Packages.Tests
         }
 
         [Fact]
-        public void Throw_IfNullOrEmpty_OfT_WithArgumentAsNull_ThrowsArgumentException()
+        public void Throw_IfNullOrEmpty_WithArgumentAsNull_ThrowsArgumentException()
         {
             // arrange
             string argument = null;
@@ -117,7 +117,7 @@ namespace Dfc.ProviderProtal.Packages.Tests
         }
 
         [Fact]
-        public void Throw_IfNullOrEmpty_OfT_WithArgumentAsEmpty_ThrowsArgumentException()
+        public void Throw_IfNullOrEmpty_WithArgumentAsEmpty_ThrowsArgumentException()
         {
             // arrange
             string argument = string.Empty;
@@ -128,7 +128,7 @@ namespace Dfc.ProviderProtal.Packages.Tests
         }
 
         [Fact]
-        public void Throw_IfNullOrEmpty_OfT_WithArgumentAsNotEmpty_DoesNotThrowArgumentException()
+        public void Throw_IfNullOrEmpty_WithArgumentAsNotEmpty_DoesNotThrowArgumentException()
         {
             // arrange
             string argument = "some value";
@@ -139,6 +139,97 @@ namespace Dfc.ProviderProtal.Packages.Tests
             try
             {
                 Throw.IfNull(argument, name);
+            }
+            catch (ArgumentException e)
+            {
+                expected = e;
+            }
+
+            // assert
+            Assert.Null(expected);
+        }
+
+
+
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsNullAndNameAsNull_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            string name = null;
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrWhiteSpace(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsNullAndNameAsEmpty_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            string name = string.Empty;
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrWhiteSpace(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsNullAndNameAsWhitespace_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            string name = " ";
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrWhiteSpace(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsNull_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = null;
+            var name = nameof(argument);
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrWhiteSpace(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsEmpty_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = string.Empty;
+            var name = nameof(argument);
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrWhiteSpace(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsWhiteSpace_ThrowsArgumentException()
+        {
+            // arrange
+            string argument = " ";
+            var name = nameof(argument);
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => Throw.IfNullOrWhiteSpace(argument, name));
+        }
+
+        [Fact]
+        public void Throw_IfNullOrWhiteSpace_WithArgumentAsNotEmpty_DoesNotThrowArgumentException()
+        {
+            // arrange
+            string argument = "some value";
+            var name = nameof(argument);
+            ArgumentException expected = null;
+
+            //act
+            try
+            {
+                Throw.IfNullOrWhiteSpace(argument, name);
             }
             catch (ArgumentException e)
             {
